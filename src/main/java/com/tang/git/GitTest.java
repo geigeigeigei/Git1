8f2473cc6e1d5300da0505bc4e9ab157044489e3
geigeigeigei <geigei@tang.com>
2022-05-11 14:10:18 +0800

    first commit

---
+++ src/main/java/com/tang/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.tang.git;
+
+/**
+ * @Company: 致远飞航
+ * @Author: 汤伟
+ * @Description: TODO
+ * @DateTime: 2022/5/11 12:11
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git02");
+    }
+}
---
+++ src/main/java/com/tang/git/GitTest01.java
@@ -0,0 +1,14 @@
+package com.tang.git;
+
+/**
+ * @Company: 致远飞航
+ * @Author: 汤伟
+ * @Description: TODO
+ * @DateTime: 2022/5/11 13:55
+ **/
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git02");
+    }
+}
