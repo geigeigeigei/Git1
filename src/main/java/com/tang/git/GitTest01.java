f94a9438b8d1572df467a4df909639ded44f6950
geigeigeigei <geigei@tang.com>
2022-05-11 14:41:41 +0800

    master commit2

--- src/main/java/com/tang/git/GitTest01.java
+++ src/main/java/com/tang/git/GitTest01.java
@@ -11,5 +11,6 @@ public class GitTest01 {
         System.out.println("hello git");
         System.out.println("hello git02");
         System.out.println("hello git03");
+        System.out.println("hello master");
     }
 }
